bba034a5ac2640d0d5cb33021f193bd3bca349fb
Daniyal Alam <daniyal.alam45@gmail.com>
2024-11-02 12:33:51 +0500

    task completed

---
+++ app.js
@@ -0,0 +1,30 @@
+var count = 0;
+
+let myDocument = document.querySelector("#counter");
+
+function inc() {
+  count++;
+  myDocument.innerText = count;
+
+  if (count > 0) {
+    myDocument.style.color = "#145a32";
+  }
+}
+
+function dec() {
+  count--;
+  myDocument.innerText = count;
+
+  if (count < 0) {
+    myDocument.style.color = "#e74c3c";
+  }
+}
+
+function reset() {
+  count = 0;
+  myDocument.innerText = count;
+
+  if (count == 0) {
+    myDocument.style.color = "#333";
+  }
+}
